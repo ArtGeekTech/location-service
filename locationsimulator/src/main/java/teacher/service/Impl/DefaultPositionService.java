219a54f6333ddf5a0f16d8c624f4887e8ee8ed05
marcogx <gx239@nyu.edu>
2018-08-17 13:30:13 -0700

    code

--- locationsimulator/src/main/java/teacher/service/Impl/DefaultPositionService.java
+++ locationsimulator/src/main/java/teacher/service/Impl/DefaultPositionService.java
@@ -1,13 +1,13 @@
 package teacher.service.Impl;
 
-import teacher.model.CurrentPosition;
-import teacher.service.PositionService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import teacher.model.CurrentPosition;
+import teacher.service.PositionService;
 
 
 @Service
@@ -15,6 +15,7 @@ public class DefaultPositionService implements PositionService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DefaultPositionService.class);
 
+    //@Autowired introduce bean with eureka
     private RestTemplate restTemplate = new RestTemplate();
 
     @Value("${com.teacher.location.ingest}")
@@ -24,6 +25,7 @@ public class DefaultPositionService implements PositionService {
         super();
     }
 
+    @HystrixCommand(fallbackMethod = "processPositionInfoFallback")
     @Override
     public void processPositionInfo(long id, CurrentPosition currentPosition, boolean exportPositionsToKml,
                                     boolean sendPositionsToIngestionService) {
@@ -32,6 +34,7 @@ public class DefaultPositionService implements PositionService {
 //            this.kmlService.updatePosition(id, currentPosition);
 //        }
 
+        // String fleetLocationIngest = "http://location-ingest"; // access by euraka server
         if (sendPositionsToIngestionService) {
             this.restTemplate.postForLocation(fleetLocationIngest + "/api/locations", currentPosition);
         }
